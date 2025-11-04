6ac57b779d4bb9003065caf08b553c9d5c24e9bc
j3rrryy <pavryab@mail.ru>
2025-11-04 15:00:13 +0300

    fix: abort incomplete multipart uploads

--- dev/create_buckets.sh
+++ dev/create_buckets.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+set -e
+
 sleep 7
 mc alias set local http://${MINIO_HOST}:${MINIO_PORT} ${MINIO_ROOT_USER} ${MINIO_ROOT_PASSWORD}
-mc mb -p local/${MINIO_S3_BUCKET}
-mc mb -p local/${MINIO_LOKI_BUCKET}
-mc ilm import local/${MINIO_S3_BUCKET} < ./lifecycle.json
+
+mc mb -p local/${MINIO_S3_BUCKET} || true
+mc mb -p local/${MINIO_LOKI_BUCKET} || true
+
+mc admin config set local api stale_uploads_expiry=24h
+mc admin service restart local --json
+
 exit 0
