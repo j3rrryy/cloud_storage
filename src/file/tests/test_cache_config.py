c1208a18224e7c5e28e3f6809b42b9a43037c4b4
j3rrryy <j3rrryy@users.noreply.github.com>
2025-11-14 14:03:30 +0000

    ruff: fix and apply formatting

--- src/file/tests/test_cache_config.py
+++ src/file/tests/test_cache_config.py
@@ -7,7 +7,7 @@ from config import setup_cache
 @patch("config.cache_config.cache")
 def test_setup_cache(mock_cache):
     setup_cache()
-    
+
     mock_cache.setup.assert_called_once_with(
         f"redis://{os.environ['REDIS_USER']}:{os.environ['REDIS_PASSWORD']}@"
         + f"{os.environ['REDIS_HOST']}:{os.environ['REDIS_PORT']}/{os.environ['REDIS_DB']}",
