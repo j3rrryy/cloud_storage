0c1b634af1d1b45eb215d02fe377c96d9bebeef3
j3rrryy <pavryab@mail.ru>
2025-12-17 12:06:56 +0300

    add smtp facade tests

---
+++ src/mail/tests/facades/test_smtp_facade.py
@@ -0,0 +1,33 @@
+import pytest
+
+from dto import BaseMailDTO, InfoMailDTO, ResetMailDTO, VerificationMailDTO
+
+from ..mocks import BROWSER, CODE, EMAIL, USER_IP, USERNAME, VERIFICATION_TOKEN
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "dto",
+    [
+        VerificationMailDTO(USERNAME, EMAIL, VERIFICATION_TOKEN),
+        InfoMailDTO(USERNAME, EMAIL, USER_IP, BROWSER),
+        ResetMailDTO(USERNAME, EMAIL, CODE),
+    ],
+)
+async def test_send_mail(dto, smtp_facade, smtp):
+    await smtp_facade.send_mail(dto)
+
+    smtp.send_message.assert_awaited_once()
+
+
+@pytest.mark.asyncio
+async def test_send_mail_no_strategy_found(smtp_facade):
+    class UnknownMailDTO(BaseMailDTO):
+        pass
+
+    dto = UnknownMailDTO(USERNAME, EMAIL)
+
+    with pytest.raises(
+        ValueError, match=f"No strategy found for {UnknownMailDTO.__name__}"
+    ):
+        await smtp_facade.send_mail(dto)
