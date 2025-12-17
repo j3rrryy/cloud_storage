8c67337a25d67b39329075b851cc77aff9181f07
j3rrryy <pavryab@mail.ru>
2025-12-17 12:58:45 +0300

    add metrics test

--- src/mail/tests/conftest.py
+++ src/mail/tests/conftest.py
@@ -1,9 +1,13 @@
+from typing import Generator
+from unittest.mock import MagicMock, patch
+
 import pytest
 from aiokafka import AIOKafkaConsumer
 from aiosmtplib import SMTP
 
 from adapters import KafkaAdapter, SMTPAdapter
 from facades import ApplicationFacade, KafkaFacade, SMTPFacade
+from metrics import PrometheusMetricsCollector
 from protocols import (
     ApplicationFacadeProtocol,
     KafkaConsumerProtocol,
@@ -54,3 +58,30 @@ def smtp_facade(smtp_adapter) -> SMTPFacadeProtocol:
 @pytest.fixture
 def application_facade(kafka_facade, smtp_facade) -> ApplicationFacadeProtocol:
     return ApplicationFacade(kafka_facade, smtp_facade)
+
+
+@pytest.fixture
+def mock_prometheus_metrics_collector() -> Generator[type[object], None, None]:
+    class MockPrometheusMetricsCollector:
+        sent_counter = MagicMock()
+        failed_counter = MagicMock()
+        processing_time = MagicMock()
+
+    with (
+        patch.object(
+            PrometheusMetricsCollector,
+            "_mails_sent_counter",
+            MockPrometheusMetricsCollector.sent_counter,
+        ),
+        patch.object(
+            PrometheusMetricsCollector,
+            "_mails_failed_counter",
+            MockPrometheusMetricsCollector.failed_counter,
+        ),
+        patch.object(
+            PrometheusMetricsCollector,
+            "_mails_processing_time",
+            MockPrometheusMetricsCollector.processing_time,
+        ),
+    ):
+        yield MockPrometheusMetricsCollector
---
+++ src/mail/tests/metrics/test_prometheus_metrics_collector.py
@@ -0,0 +1,34 @@
+from enums import MailTypes
+from metrics import PrometheusMetricsCollector
+
+
+def test_record_success(mock_prometheus_metrics_collector):
+    metrics_collector = mock_prometheus_metrics_collector
+    topic = MailTypes.VERIFICATION.name
+
+    PrometheusMetricsCollector.record_success(topic)
+
+    metrics_collector.sent_counter.labels.assert_called_once_with(topic)
+    metrics_collector.failed_counter.labels.assert_not_called()
+    metrics_collector.processing_time.time.assert_not_called()
+
+
+def test_record_failure(mock_prometheus_metrics_collector):
+    metrics_collector = mock_prometheus_metrics_collector
+    topic = MailTypes.VERIFICATION.name
+
+    PrometheusMetricsCollector.record_failure(topic)
+
+    metrics_collector.sent_counter.labels.assert_not_called()
+    metrics_collector.failed_counter.labels.assert_called_once_with(topic)
+    metrics_collector.processing_time.time.assert_not_called()
+
+
+def test_record_processing_time(mock_prometheus_metrics_collector):
+    metrics_collector = mock_prometheus_metrics_collector
+
+    PrometheusMetricsCollector.record_processing_time()
+
+    metrics_collector.sent_counter.labels.assert_not_called()
+    metrics_collector.failed_counter.labels.assert_not_called()
+    metrics_collector.processing_time.time.assert_called_once()
