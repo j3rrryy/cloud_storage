a39fd521da008b1c3c2892fc7cf6607ffe178c0f
j3rrryy <pavryab@mail.ru>
2026-01-21 23:50:49 +0300

    chore(gateway): use folders for proto files

--- src/gateway/proto/__init__.py
+++ src/gateway/proto/__init__.py
@@ -1,4 +1,4 @@
-from .auth_pb2_grpc import AuthStub
-from .file_pb2_grpc import FileStub
+from .auth import AuthStub, auth_pb2
+from .file import FileStub, file_pb2
 
-__all__ = ["AuthStub", "FileStub"]
+__all__ = ["AuthStub", "auth_pb2", "FileStub", "file_pb2"]
---
+++ src/gateway/proto/file/__init__.py
@@ -0,0 +1,3 @@
+from .file_pb2_grpc import FileStub
+
+__all__ = ["FileStub"]
